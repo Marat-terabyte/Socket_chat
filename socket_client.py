d7578b52f9a590380bf547e6dedc21806452dec9
Marat-terabyte <marat.amzph21@gmail.com>
2021-06-23 18:03:56 +0300

    DEBUG

--- socket_client.py
+++ socket_client.py
@@ -4,22 +4,21 @@ import threading
 
 client = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 
-client.connect((input(str('Введите IP адрес хоста')) , 8000))
-
+client.connect((input(str('Введите IP адрес хоста:')) , 8000))
 
 def listen_server():
 	while True:
-		data = client.recv(2048)
-		print(data.decode())
+		data = client.recv(2048).decode()
+		#print(data)
 
 
 
 def start_client():
 	listen_thread = threading.Thread(target = listen_server)
 	listen_thread.start()
-	while True:
-		client.send(input(':::').encode())
 
+	while True:
+		client.send(input('::::').encode())
 
 
 if __name__ == '__main__':
